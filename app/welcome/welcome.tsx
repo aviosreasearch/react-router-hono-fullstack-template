f0c80426742b4ff8c9f15bf291c5dc46418e7159
aviosreasearch <lafamiliaestodo13@icloud.com>
2026-08-16 17:14:35 -0600

    update hero image to new Avios vial design

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -102,7 +102,7 @@ Verify a COA
     <div className="relative hidden min-h-[520px] overflow-hidden rounded-3xl lg:block">
       <div
         className="absolute inset-0 bg-cover bg-[72%_center] bg-no-repeat"
-        style={{ backgroundImage: 'url("/avios-hero-glp3.png")' }}
+        style={{ backgroundImage: 'url("/avios-hero-vial-v2.png")' }}
       />
     </div>
   </div>
