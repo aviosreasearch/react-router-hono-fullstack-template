bfe8218d13ed576ad57fe3e5d28565296d46ff43
aviosreasearch <lafamiliaestodo13@icloud.com>
2026-08-23 15:25:23 -0600

    Add GHK-CU product lot data

--- app/data/public-product-lots.ts
+++ app/data/public-product-lots.ts
@@ -19,6 +19,7 @@ export const publicProductLots = {
   "mots-c": [] as PublicProductLot[],
   "glp3-r": [] as PublicProductLot[],
   "nad-plus": [] as PublicProductLot[],
+  "ghk-cu": [] as PublicProductLot[],
 };
 
 export type ProductSlug = keyof typeof publicProductLots;
