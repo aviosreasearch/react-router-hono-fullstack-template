c13d23217b6dc972e7c295841c73cf37cd50feb5
aviosreasearch <lafamiliaestodo13@icloud.com>
2026-08-23 18:13:40 -0600

    Fix MOTS-C LotDocumentation import

--- app/routes/products.mots-c.tsx
+++ app/routes/products.mots-c.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { LotDocumentation } from "../components/LotDocumentation";
+import LotDocumentation from "../components/LotDocumentation";
 
 const sizes = [
   {
