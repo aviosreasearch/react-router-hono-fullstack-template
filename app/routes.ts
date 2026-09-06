3ab5529599547599ead2147c24b04d695393883e
aviosreasearch <lafamiliaestodo13@icloud.com>
2026-09-05 20:31:16 -0600

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -21,7 +21,7 @@ export default [
   route("products/ss-31", "routes/products.ss-31.tsx"),
   route("products/epithalon", "routes/products.epithalon.tsx"),
 
-  route("products/bpc157", "routes/products.bpc-157.tsx"),
+  route("products/bpc157", "routes/products.bpc157.tsx"),
   route(
     "products/wolverine-blend",
     "routes/products.wolverine-blend.tsx"
